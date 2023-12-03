9b23cf97249d1010a9d5cd84189849357d0b1135
HelplessSoldier <patrick1234143211@gmail.com>
2023-12-03 13:51:09 -0800

    have all gear ratio pairs i think

--- 03_gearRatios/03_gearRatios.js
+++ 03_gearRatios/03_gearRatios.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const data = fs.readFileSync("./data.txt", "utf8");
 
 console.log(sumPartNumbers(data));
+sumGearRatios(data);
 
 // part 1
 function sumPartNumbers(data) {
@@ -24,9 +25,34 @@ function getGearRatios(dataArray) {
   const arrWidth = dataArray[0].length;
   const searchDirections = getSearchDirections();
 
+  let allPotentialGearRatios = [];
   for (let x = 0; x < arrHeight; x++) {
-    for (let y = 0; y < arrWidth; y++) { }
+    for (let y = 0; y < arrWidth; y++) {
+      const currentChar = dataArray[x][y];
+
+      if (currentChar === '*') {
+        let adjecentNums = [];
+        for (let direction of searchDirections) {
+          const searchX = x + direction[0];
+          const searchY = y + direction[1];
+          if (inBounds(searchX, searchY, arrWidth, arrHeight)) {
+            if (isNumerical(dataArray[searchX][searchY])) {
+              const num = getWholeNum(dataArray, searchX, searchY);
+              if (adjecentNums.includes(num)) {
+                continue;
+              } else {
+                adjecentNums.push(num);
+              }
+            }
+          }
+        }
+        if (adjecentNums.length === 2) {
+          allPotentialGearRatios.push(adjecentNums);
+        }
+      }
+    }
   }
+  console.log(allPotentialGearRatios);
 }
 
 function getPartNumbers(dataArray) {
@@ -112,6 +138,39 @@ function isSpecialChar(char) {
     return true;
   }
   return false;
+}
+
+function getWholeNum(dataArray, x, y) {
+  const arrHeight = dataArray.length;
+  const arrWidth = dataArray[0].length;
+  // move to leftmost numerical char
+  for (; ;) {
+    if (!inBounds(x, y - 1, arrWidth, arrHeight)) {
+      break;
+    }
+    const checkChar = dataArray[x][y - 1];
+    if (!isNumerical(checkChar)) {
+      break;
+    }
+    y--;
+  }
+
+  // build whole num
+  let res = '';
+  for (; ;) {
+    if (!inBounds(x, y + 1, arrWidth, arrHeight)) {
+      break;
+    }
+    const currentChar = dataArray[x][y];
+    if (isNumerical(currentChar)) {
+      res = res + currentChar;
+    } else {
+      break;
+    }
+    y++;
+  }
+
+  return Number(res);
 }
 
 function isNumerical(char) {
